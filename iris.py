cb787830093ae8a6e58b0d34e423537429a0314e
ChristieOng <109711113+ChristieOng@users.noreply.github.com>
2022-07-21 15:49:57 +0800

    Update iris.py

--- iris.py
+++ iris.py
@@ -8,6 +8,9 @@ st.write("""
 This app predicts the **Iris flower** type!
 """)
 
+from PIL import Image
+image = Image.open('irisflower.jpg')
+
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
